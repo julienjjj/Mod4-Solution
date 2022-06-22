0e8413bc040314df8e99448c2845d17ed37e385d
julienjjj <106631578+julienjjj@users.noreply.github.com>
2022-06-22 11:05:42 -0400

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -26,8 +26,8 @@
 (function(window){
 var byeSpeaker = {};
 var speakWord = "Good Bye";
-byeSpeaker.speak = function(name) {
-  console.log(speakWord + " " + name);
+byeSpeaker.speak = function(names) {
+  console.log(speakWord + " " + names);
 }
 window.byeSpeaker = byeSpeaker;
 })(window);
